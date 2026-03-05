c1dcc93c6684ba82baa19c902f8297aabff46625
bakalskaya <mobilkaxxx@icloud.com>
2026-03-05 18:26:59 +0300

    StudentInfo Widget creating

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,58 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class StudentInfo extends StatelessWidget {
+  final String fullName;
+  final String group;
+  final String studentId;
+
+  final Color backgroundColor;
+  final Color textColor;
+  final double fontSize;
+  final double borderRadius;
+  final double rightMargin;
+
+  const StudentInfo({
+    super.key,
+    required this.fullName,
+    required this.group,
+    required this.studentId,
+    this.backgroundColor = Colors.white,
+    this.textColor = Colors.blue,
+    this.fontSize = 20.0,
+    this.borderRadius = 20.0,
+    this.rightMargin = 100.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: rightMargin),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+      child: Text(
+        'ФИО: $fullName\n\n'
+            'Номер группы: $group\n\n'
+            'Номер студенческого билета: $studentId',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: textColor,
+          fontSize: fontSize,
+        ),
+      ),
+    );
+  }
+}
+
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
 
   @override
   Widget build(BuildContext context) {
@@ -37,36 +85,12 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
 
-            Padding(
-              padding: const EdgeInsets.only(right: 100),
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                margin: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const <Widget>[
-                    Text(
-                      'ФИО: Бакальская Софья Юрьевна',
-                      style: TextStyle(
-                        color: Colors.red,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Номер группы: ИКБО-06-22',
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Номер студенческого билета: 22И1580',
-                    ),
-                  ],
-                ),
-              ),
+            const StudentInfo(
+              fullName: 'Бакальская Софья Юрьевна',
+              group: 'ИКБО-06-22',
+              studentId: '22И1580',
+              backgroundColor: Colors.white,
+              textColor: Colors.red,
             ),
 
             ElevatedButton(
