1d728aa663624c6de6c75f6754f6b8c1ba33fd04
bakalskaya <mobilkaxxx@icloud.com>
2026-03-06 05:04:49 +0300

    Create a Navigation

--- lib/core/navigation/app_routes.dart
+++ lib/core/navigation/app_routes.dart
@@ -1,5 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../features/jobs/jobs_screen.dart';
+import '../../features/job-detail/job_detail_screen.dart';
+import '../../features/favorites/favorites_screen.dart';
+import '../models/job.dart';
+
 class AppRoutes {
   static const jobs = '/';
-  static const jobDetail = '/job-detail';
   static const favorites = '/favorites';
+  static const jobDetail = '/job-detail';
+
+  static Route<dynamic> onGenerate(RouteSettings settings) {
+    switch (settings.name) {
+      case jobs:
+        return MaterialPageRoute(
+          builder: (_) => const JobsScreen(),
+        );
+
+      case favorites:
+        return MaterialPageRoute(
+          builder: (_) => const FavoritesScreen(),
+        );
+
+      case jobDetail:
+        final job = settings.arguments as Job;
+
+        return MaterialPageRoute(
+          builder: (_) => JobDetailScreen(),
+          settings: RouteSettings(arguments: job),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const JobsScreen(),
+        );
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'core/navigation/app_routes.dart';
-
-import 'features/jobs/jobs_screen.dart';
-import 'features/job-detail/job_detail_screen.dart';
-import 'features/favorites/favorites_screen.dart';
 import 'features/favorites/favorites_provider.dart';
 
 void main() {
@@ -22,14 +18,8 @@ class JobsApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Job Aggregator',
-
         initialRoute: AppRoutes.jobs,
-
-        routes: {
-          AppRoutes.jobs: (_) => const JobsScreen(),
-          AppRoutes.jobDetail: (_) => const JobDetailScreen(),
-          AppRoutes.favorites: (_) => const FavoritesScreen(),
-        },
+        onGenerateRoute: AppRoutes.onGenerate,
       ),
     );
   }
